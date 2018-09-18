deab343e2ff0e78afe0f6bd7562ae963f81bbc91
Flatiron School <flatironschool@Flatirons-MacBook-Air-2.local>
2018-09-18 15:15:55 -0400

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -3,6 +3,12 @@
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
-welcome
-character = get_character_from_user
-show_character_movies(character)
+# welcome
+# character = get_character_from_user
+# show_character_movies(character)
+
+
+response_string = RestClient.get('http://www.swapi.co/api/people/')
+response_hash = JSON.parse(response_string)
+binding.pry
+puts "end"
